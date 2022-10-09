e8f217431da172ac5939c1fb81625b959f1bccd4
timohuennebeck <hello@timohuennebeck.com>
2022-10-09 21:21:09 +0200

    adjust header hover

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,17 +7,17 @@ import InputField from "../../components/InputField/InputField";
 function HomePage() {
     return (
         <>
-            <article className="homepage">
-                <div className="homepage__sec">
+            <article className="home">
+                <div className="home__sec">
                     <InputField content="Search Leads..." />
-                    <div className="homepage__sec-btn">
+                    <div className="home__sec-btn">
                         <Button
                             content="IMPORT LEADS"
                             backgroundColor="#1C3F32"
                         />
                         <Button content="DELETE" backgroundColor="#E43A07" />
                     </div>
-                    <div className="homepage__sec-leads">
+                    <div className="home__sec-leads">
                         <LeadInformation />
                     </div>
                 </div>
