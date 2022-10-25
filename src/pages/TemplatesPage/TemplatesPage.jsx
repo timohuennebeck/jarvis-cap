560026fd414fbac05b67c130ad91fcefa0006f99
timohuennebeck <hello@timohuennebeck.com>
2022-10-25 16:28:33 +0200

    make templates page responsive

--- src/pages/TemplatesPage/TemplatesPage.jsx
+++ src/pages/TemplatesPage/TemplatesPage.jsx
@@ -1,7 +1,16 @@
+import TemplatePreview from "../../components/TemplatePreview/TemplatePreview";
 import "./TemplatesPage.scss";
 
 function TemplatesPage() {
-    return <div>TemplatesPage</div>;
+    return (
+    <article className="templates">
+        <div className="templates__sec">
+            <TemplatePreview />
+            <TemplatePreview />
+            <TemplatePreview />
+        </div>
+    </article>
+        )
 }
 
 export default TemplatesPage;
