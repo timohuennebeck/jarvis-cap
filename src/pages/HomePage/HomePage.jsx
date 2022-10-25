4e0a46a3327ed7cf9ac91d913c39d2b503427ac1
timohuennebeck <hello@timohuennebeck.com>
2022-10-25 12:01:54 +0200

    move homepage to leads page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,24 +1,9 @@
 import "./HomePage.scss";
 
-import LeadInformation from "../../components/LeadInformation/LeadInformation";
-import Button from "../../components/Button/Button";
-import InputField from "../../components/InputField/InputField";
-
 function HomePage() {
     return (
         <>
-            <article className="home">
-                <div className="home__sec">
-                    <InputField content="Search Leads..." />
-                    <div className="home__sec-btn">
-                        <Button content="IMPORT LEADS" backgroundColor="#1C3F32" />
-                        <Button content="DELETE" backgroundColor="#E43A07" />
-                    </div>
-                    <div className="home__sec-leads">
-                        <LeadInformation />
-                    </div>
-                </div>
-            </article>
+        <div>HomePage</div>
         </>
     );
 }
--- src/pages/LeadsPage/LeadsPage.jsx
+++ src/pages/LeadsPage/LeadsPage.jsx
@@ -1,5 +1,26 @@
+import "./LeadsPage.scss";
+
+import LeadInformation from "../../components/LeadInformation/LeadInformation";
+import Button from "../../components/Button/Button";
+import InputField from "../../components/InputField/InputField";
+
 function LeadsPage() {
-    return <div>LeadsPage</div>;
+    return (
+        <>
+            <article className="leads">
+                <div className="leads__sec">
+                    <InputField content="Search Leads..." />
+                    <div className="leads__sec-btn">
+                        <Button content="IMPORT LEADS" backgroundColor="#1C3F32" />
+                        <Button content="DELETE" backgroundColor="#E43A07" />
+                    </div>
+                    <div className="leads__sec-indv">
+                        <LeadInformation />
+                    </div>
+                </div>
+            </article>
+        </>
+    );
 }
 
 export default LeadsPage;
