458d6e785d6b4b308baeee8a7c5e3f348e8f858c
timohuennebeck <hello@timohuennebeck.com>
2022-10-27 15:34:51 +0200

    refactor app.scss file for background

--- src/pages/ReviewPage/ReviewPage.jsx
+++ src/pages/ReviewPage/ReviewPage.jsx
@@ -6,17 +6,15 @@ import ReviewTextPage from "../ReviewTextPage.jsx/ReviewTextPage";
 function ReviewPage() {
     return (
         <article className="review">
-            <div className="review__sec">
-                <div className="review__sec-leads">
-                    <LeadInformationMinimized />
-                    <LeadInformationMinimized />
-                    <LeadInformationMinimized />
-                    <LeadInformationMinimized />
-                    <LeadInformationMinimized />
-                </div>
-                <div className="review__sec-content">
-                    <ReviewTextPage />
-                </div>
+            <div className="review__leads">
+                <LeadInformationMinimized />
+                <LeadInformationMinimized />
+                <LeadInformationMinimized />
+                <LeadInformationMinimized />
+                <LeadInformationMinimized />
+            </div>
+            <div className="review__content">
+                <ReviewTextPage />
             </div>
         </article>
     );
--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -6,38 +6,36 @@ import "./SettingsPage.scss";
 function SettingsPage() {
     return (
         <article className="settings">
-            <div className="settings__sec">
-                <div className="settings__sec-information">
-                    <h2 className="settings__sec-information-header">Personal Information</h2>
-                    <p className="settings__sec-information-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
-                        Lorem ipsum dolor sit amet...
-                    </p>
-                    <div className="settings__sec-information-input">
-                        <div className="settings__sec-information-input-indv">
-                            <InputField placeholder="Full Name" />
-                        </div>
-                        <div className="settings__sec-information-input-indv">
-                            <InputField placeholder="Last Name" />
-                        </div>
+            <div className="settings__information">
+                <h2 className="settings__information-header">Personal Information</h2>
+                <p className="settings__information-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Lorem ipsum
+                    dolor sit amet...
+                </p>
+                <div className="settings__information-input">
+                    <div className="settings__information-input-indv">
+                        <InputField placeholder="Full Name" />
                     </div>
-                    <div className="settings__sec-link">
-                        <ButtonElement content="SAVE SETTINGS" backgroundColor="#000000" />
+                    <div className="settings__information-input-indv">
+                        <InputField placeholder="Last Name" />
                     </div>
                 </div>
-                <div className="settings__sec-delete">
-                    <h2 className="settings__sec-delete-header">Delete Personal Account</h2>
-                    <p className="settings__sec-delete-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
-                        Lorem ipsum dolor sit amet...
-                    </p>
-                    <div className="settings__sec-link">
-                        <ButtonElement content="DELETE ACCOUNT" backgroundColor="#E43A07" />
-                    </div>
+                <div className="settings__link">
+                    <ButtonElement content="SAVE SETTINGS" backgroundColor="#000000" />
+                </div>
+            </div>
+            <div className="settings__delete">
+                <h2 className="settings__delete-header">Delete Personal Account</h2>
+                <p className="settings__delete-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Lorem ipsum
+                    dolor sit amet...
+                </p>
+                <div className="settings__link">
+                    <ButtonElement content="DELETE ACCOUNT" backgroundColor="#E43A07" />
                 </div>
             </div>
         </article>
