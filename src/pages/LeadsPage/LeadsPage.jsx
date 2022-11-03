36e3eb5172a090a3893b1f36d9dc63eda1f1df4e
timohuennebeck <hello@timohuennebeck.com>
2022-11-03 15:52:58 +0100

    add csv upload

--- src/pages/LeadsPage/LeadsPage.jsx
+++ src/pages/LeadsPage/LeadsPage.jsx
@@ -9,7 +9,6 @@ import DeleteNotification from "../../components/DeleteNotification/DeleteNotifi
 import { getLeads } from "../../utils/api";
 
 // libraries
-import ReactModal from "react-modal";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
@@ -20,7 +19,11 @@ export default function LeadsPage() {
     const [leads, setLeads] = useState([]);
     const [parsedData, setParsedData] = useState([]);
 
-    const [modelIsOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        getLeads().then((resp) => {
+            setLeads(resp.data);
+        });
+    }, []);
 
     const handleSubmit = (event) => {
         Papa.parse(event.target.files[0], {
@@ -28,6 +31,7 @@ export default function LeadsPage() {
             skipEmptyLines: true,
             complete: function (results) {
                 setParsedData(results.data);
+                console.log(results.data);
             },
         });
     };
@@ -55,40 +59,6 @@ export default function LeadsPage() {
         });
     }, [parsedData]);
 
-    // useEffect(() => {
-    //     const addInputData = parsedData.map((person) => ({
-    //         status: person.status,
-    //         first_name: person.first_name,
-    //         last_name: person.last_name,
-    //         position: person.position,
-    //         email: person.email,
-    //         phone: person.phone,
-    //         image_url: person.image_url,
-    //         linked_in: person.linked_in,
-    //         business_name: person.business_name,
-    //         postcode: person.postcode,
-    //         icebreaker: person.icebreaker,
-    //         paragraph_one: person.paragraph_one,
-    //         paragraph_two: person.paragraph_two,
-    //         paragraph_three: person.paragraph_three,
-    //         call_to_action: person.call_to_action,
-    //     }));
-    // }, [parsedData]);
-
-    function openModal() {
-        setIsOpen(true);
-    }
-
-    function closeModal() {
-        setIsOpen(false);
-    }
-
-    useEffect(() => {
-        getLeads().then((resp) => {
-            setLeads(resp.data);
-        });
-    }, []);
-
     return (
         <>
             <article className="leads">
@@ -99,7 +69,7 @@ export default function LeadsPage() {
                         backgroundColor="#FFF"
                         fontColor="#000"
                     />
-                    <ButtonElement content="DELETE" backgroundColor="#E43A07" onClick={openModal} />
+                    <ButtonElement content="DELETE" backgroundColor="#E43A07" />
                     <input type="file" name="file" accept=".csv" onChange={handleSubmit} />
                 </div>
                 <div className="leads__indv">
@@ -115,9 +85,6 @@ export default function LeadsPage() {
                         );
                     })}
                 </div>
-                <ReactModal isOpen={modelIsOpen} onRequestClose={closeModal}>
-                    <DeleteNotification closeModal={closeModal} />
-                </ReactModal>
             </article>
         </>
     );
