1add8c71d763bda42775ab742f964136297966b9
timohuennebeck <hello@timohuennebeck.com>
2022-11-04 15:35:54 +0100

    add editor page

--- src/pages/EditorPage/EditorPage.jsx
+++ src/pages/EditorPage/EditorPage.jsx
@@ -1,15 +1,23 @@
 import Handlebars from "handlebars/dist/cjs/handlebars";
 
 const clTemplate = `
-    <p>Timo Huennebeck
+    <p>
+    {{user_first_name}} {{user_last_name}}
+    </p>
+    <p>{{user_street_name}}</p>
+    <p>{{user_city}}</p>
+    <p>{{user_state}} {{user_postcode}}</p>
+<br />
     <p>{{date}}</p>
 <br />
     <p>
     {{first_name}} {{last_name}}
     </p>
-    <p>{{business_name}}</p>
+    <p>{{position}}</p>
+    <p>{{company}}</p>
     <p>{{street_name}}</p>
-    <p>{{postcode}}</p>
+    <p>{{city}}</p>
+    <p>{{state}} {{postcode}}</p>
 <br />
     <p>Dear {{first_name}},</p>
 <br />
@@ -24,18 +32,27 @@ const clTemplate = `
     <p>{{call_to_action}}</p>
 <br />
     <p>Best regards,</p>
-    <p>Timo</p>
+    <p>{{first_name}}</p>
 `;
 
 const template = Handlebars.compile(clTemplate);
 
 export default function EditorPage() {
     const data = {
+        user_first_name: "{{user_first_name}}",
+        user_last_name: "{{user_last_name}}",
+        user_street_name: "{{user_street_name}}",
+        user_city: "{{user_city}}",
+        user_state: "{{user_state}}",
+        user_postcode: "{{user_postcode}}",
         date: "{{date}}",
         first_name: "{{first_name}}",
         last_name: "{{last_name}}",
-        business_name: "{{business_name}}",
+        position: "{{position}}",
+        company: "{{company}}",
         street_name: "{{street_name}}",
+        city: "{{city}}",
+        state: "{{state}}",
         postcode: "{{postcode}}",
         icebreaker: "{{icebreaker}}",
         paragraph_one: "{{paragraph_one}}",
