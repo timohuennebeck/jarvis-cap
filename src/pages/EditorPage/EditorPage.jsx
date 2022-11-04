59d8d3baeae730b403e68a1b69ec075455e6260c
timohuennebeck <hello@timohuennebeck.com>
2022-11-04 11:17:10 +0100

    fix bug of leads not updating in review page

--- src/pages/EditorPage/EditorPage.jsx
+++ src/pages/EditorPage/EditorPage.jsx
@@ -1,24 +1,3 @@
-// import Handlebars from "handlebars";
-
-// const hbr = `
-// <p>
-// {{date}}
-// {{business_name}}
-// </p>
-// `;
-
-// const template = Handlebars.compile(hbr);
-
 export default function EditorPage() {
-    // const data = {
-    //     date: "{{date}}",
-    //     business_name: "{{business_name}}",
-    //     first_name: "{{first_name}}",
-    //     last_name: "{{last_name}}"
-    // };
-    return (
-        <div>
-            {/* <div dangerouslySetInnerHTML={{ __html: template(data) }} /> */}
-        </div>
-    );
+    return <div></div>;
 }
