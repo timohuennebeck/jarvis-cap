6eb134688ff6802e97d4afd69814090b0aa6e77e
timohuennebeck <hello@timohuennebeck.com>
2022-11-05 14:09:48 +0100

    add log-in and register page

--- src/App.js
+++ src/App.js
@@ -21,20 +21,22 @@ import AddNewLead from "./components/AddNewLead/AddNewLead";
 import EditorPage from "./pages/EditorPage/EditorPage";
 import LoadingPage from "./pages/LoadingPage/LoadingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
     return (
         <div className="app">
             <BrowserRouter>
-                {/* <Header /> */}
+                <Header />
                 <div className="app__structure">
-                    {/* <div className="app__structure-sidebar">
+                    <div className="app__structure-sidebar">
                         <SBNavigation />
                         <SBTemplates />
-                    </div> */}
+                    </div>
                     <div className="app__structure-routes">
                         <Routes>
                             <Route path="/log-in" element={<LoginPage />} />
+                            <Route path="/register" element={<RegisterPage />} />
                             <Route path="/loading" element={<LoadingPage />} />
                             <Route path="/" element={<HomePage />} />
                             <Route path="/leads" element={<LeadsPage />} />
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,6 +4,7 @@ import UserImg from "../../assets/icons/user.png";
 import PasswordImg from "../../assets/icons/password.png";
 import LogoImg from "../../assets/images/jarvis-logo.png";
 import ButtonElement from "../../components/ButtonElement/ButtonElement";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
     return (
@@ -11,7 +12,14 @@ export default function LoginPage() {
             <div className="log-in__header">
                 <img className="log-in__header-img" src={LogoImg} alt="logo" />
                 <div className="log-in__header-content">
-                    <span className="log-in__header-content-brand">jarvis</span>
+                    <div class="log-in__header-content-container">
+                        <ul class="log-in__header-content-container-list">
+                            <li class="log-in__header-content-container-list-item">welcome</li>
+                            <li class="log-in__header-content-container-list-item">to the</li>
+                            <li class="log-in__header-content-container-list-item">world</li>
+                            <li class="log-in__header-content-container-list-item">of jarvis!</li>
+                        </ul>
+                    </div>
                     <p className="log-in__header-content-paragraph">
                         Please log in to get started...
                     </p>
@@ -23,8 +31,16 @@ export default function LoginPage() {
                 <LoginField img={UserImg} placeholder="Username" />
                 <LoginField img={PasswordImg} placeholder="Password" type="password" />
                 <p className="log-in__input-forgot">Forgot Password?</p>
-                <div className="log-in__input-btn">
-                    <ButtonElement link="/loading" content="Log In" backgroundColor="#000" />
+                <div className="log-in__input-user">
+                    <Link to="/loading">
+                        <button className="log-in__input-user-link">Log In</button>
+                    </Link>
+                    <p className="log-in__input-user-existing-user">
+                        Don't have an account?{" "}
+                        <Link to="/register" className="log-in__input-user-existing-user-link">
+                            Register here.
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
