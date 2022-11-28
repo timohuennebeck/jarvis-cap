eebe9c5c3ada4fc6cf98190a8c3d4f82a8316a91
timohuennebeck <hello@timohuennebeck.com>
2022-11-28 15:22:52 +0100

    add mobile banner

--- src/components/NavBarModal/NavBarModal.jsx
+++ src/components/NavBarModal/NavBarModal.jsx
@@ -1,12 +1,23 @@
 import "./NavBarModal.scss";
 
+import dashhboardImg from "../../assets/icons/collapse-alt.svg";
+import trackerImg from "../../assets/icons/tasksvg.svg";
+import leadsImg from "../../assets/icons/users.svg";
+import companiesImg from "../../assets/icons/briefcase.svg";
+import reviewImg from "../../assets/icons/eye.svg";
+
 import ButtonWithLink from "../ButtonWithLink/ButtonWithLink";
 
-import leadsImg from "../../assets/icons/users.svg"
-
-export default function NavBarModal({closeModal}) {
-    return <div className="nav-modal">
-        <p onClick={closeModal}>Close</p>
-        <ButtonWithLink name="Leads" img={leadsImg}/>
-    </div>;
+export default function NavBarModal({ closeModal }) {
+    return (
+        <div className="nav-modal">
+            <div className="nav-modal__links" onClick={closeModal}>
+                <ButtonWithLink link="/construction" name="Dashboard" img={dashhboardImg} />
+                <ButtonWithLink link="/tracker" name="Tracker" img={trackerImg} />
+                <ButtonWithLink link="/leads" name="Leads" img={leadsImg} />
+                <ButtonWithLink link="/construction" name="Companies" img={companiesImg} />
+                <ButtonWithLink link="/review/1" name="Review" img={reviewImg} />
+            </div>
+        </div>
+    );
 }
