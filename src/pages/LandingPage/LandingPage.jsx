4143f7c8a78d0d4f8c8844051112b55b1c4bf211
timohuennebeck <hello@timohuennebeck.com>
2023-01-10 15:13:04 +0100

    change name

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import "./LandingPage.scss";
 
 // images
-import jarvisLogo from "../../assets/images/jarvis-logo.png";
+import ireliaLogo from "../../assets/images/irelia-logo.png";
 
 // libraries
 import { useAuth0 } from "@auth0/auth0-react";
@@ -11,26 +11,33 @@ export default function LandingPage() {
 
     return (
         <div className="landing">
-            <div className="landing__header">
-                <div className="landing__header-buttons">
+            <div className="landing__ctn">
+                <div className="landing__ctn-access">
+                    <p>Don't have an account? Log in using a sample account:</p>
+                    <p>Email: beta@irelia.com and PW: fnQ6RT6qT?M@3ef9</p>
+                </div>
+                <div className="landing__ctn-indv">
+                    <p className="landing__ctn-indv-version">Alpha Version 1.8</p>
+                    <img className="landing__ctn-indv-logo" src={ireliaLogo} alt="" />
+                    <p className="landing__ctn-indv-welcome">Welcome to Irelia!</p>
+                    <p className="landing__ctn-indv-account">
+                        Log in with a Google account or sign up using email to continue.
+                    </p>
+                </div>
+                <div className="landing__ctn-login">
                     <button
-                        onClick={() => loginWithRedirect({screen_hint: "signup"})}
-                        className="landing__header-buttons-indv"
+                        onClick={() => loginWithRedirect({ screen_hint: "signup" })}
+                        className="landing__ctn-login-buttons"
                     >
                         Sign Up
                     </button>
                     <button
                         onClick={() => loginWithRedirect()}
-                        className="landing__header-buttons-indv"
+                        className="landing__ctn-login-buttons"
                     >
                         Log In
                     </button>
                 </div>
-                <p className="landing__header-version">Beta Version 1.1</p>
-            </div>
-            <div className="landing__img">
-                <p className="landing__img-version">Beta Version 1.1</p>
-                <img className="landing__img-mobile" src={jarvisLogo} alt="" />
             </div>
         </div>
     );
